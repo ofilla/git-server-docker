c0c11fa49eb62cc5ba436a45f1cd3633f41ed44a
Oliver Filla <ocl@hotmail.de>
2025-02-18 16:29:25 +0100

    start.sh: Replace relative paths with absolute paths

--- start.sh
+++ start.sh
@@ -2,23 +2,22 @@
 
 ssh-keygen -A
 
-# If there is some public key in keys folder
+# If there are some public keys in keys folder
 # then it copies its contain in authorized_keys file
 if [ "$(ls -A /git-server/keys/)" ]; then
   cd /home/git
-  cat /git-server/keys/*.pub > .ssh/authorized_keys
-  chown -R git:git .ssh
-  chmod 700 .ssh
-  chmod -R 600 .ssh/*
+  cat /git-server/keys/*.pub > /home/git/.ssh/authorized_keys
+  chown -R git:git /home/git/.ssh
+  chmod 700 /home/git/.ssh
+  chmod -R 600 /home/git/.ssh/*
 fi
 
 # Checking permissions and fixing SGID bit in repos folder
 # More info: https://github.com/jkarlosb/git-server-docker/issues/1
 if [ "$(ls -A /git-server/repos/)" ]; then
-  cd /git-server/repos
-  chown -R git:git .
-  chmod -R ug+rwX .
-  find . -type d -exec chmod g+s '{}' +
+  chown -R git:git /git-server/repos
+  chmod -R ug+rwX /git-server/repos
+  find /git-server/repos -type d -exec chmod g+s '{}' +
 fi
 
 # -D flag avoids executing sshd as a daemon
